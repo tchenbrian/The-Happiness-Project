6a3b6860cf54232d5606d2fa418bbf6cd673e7d4
tchenbrian <brian.715218@nv.ccsd.net>
2017-05-17 15:32:27 -0700

    final
    final

--- scripts/quizfive.js
+++ scripts/quizfive.js
@@ -1,8 +1,6 @@
 function submitAnswers() {
-    
-    var eScore = 0;
-    var iScore = 0;
-    var total = 10;
+  var total = 10;
+  var score = 0;
   
   //Get user input
     
@@ -20,7 +18,7 @@ function submitAnswers() {
   
     
   // Validation  
-  /* for(var i = 1; i <= total; i++) {
+ /* for(var i = 1; i <= total; i++) {
     if(eval('q' + i) === null || eval('q' + i) == '' ) {
       alert('Please complete question ' + i);
       return false;
@@ -28,23 +26,27 @@ function submitAnswers() {
   } */
   
   // Set correct answers
-var extrovert =["Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree"];
-var introvert = ["Agree","Agree","Agree","Agree","Agree","Agree","Agree","Agree","Agree","Agree" ];
+  var answers = ["Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree","Disagree"];
   
   for(var i = 1; i <= total; i++) {
     // Check answers
-    if (eval('q' + i) == extrovert[i - 1]) {
-      eScore++;
-    }  
-  } 
-    for(var i = 1; i <= total; i++) {
-    // Check answers
-    if (eval('q' + i) == introvert[i - 1]) {
-      iScore++;
-    }  
+    if (eval('q' + i) == answers[i - 1]) {
+      score++;
+    }
   } 
 
-  return false;    
+  // Display results
+  var results = document.getElementById('results');
+  results.innerHTML = '<h3>You scored <span>' + score + '</span> out of <span>' + total + '.' + '</span></h3>';
+  
+    if (score >= 5) {
+        results.innerHTML += '<h3>Based on your responses, you’re an extrovert. You relish social life and are energized by interacting with friends and strangers alike. You’re assertive, go-getting, and able to seize the day. You’re great at thinking on your feet and relatively comfortable with conflict. Given the choice, you usually prefer more stimulating environments that give you frequent opportunities to see and speak with others. When you’re in quiet environments, you’re prone to feeling bored and restless. You’re actively engaged in the world around you and at your best when you tap into its energy. </h3>';
+    } else {
+        results.innerHTML += '<h3>Based on your responses, you are an introvert. Given the choice, you’ll devote your social energy to a small group of people you care about most, preferring a glass of wine with a close friend to a party full of strangers. You think before you speak, have a more deliberate approach to risk, and enjoy solitude. You feel energized when focusing deeply on a subject or activity that really interests you. When you’re in overly stimulating environments (too loud, too crowded, etc.) you tend to feel overwhelmed. You seek out environments of peace, sanctuary, and beauty; you have an active inner life and are at your best when you tap into its riches.</h3>';
+    }
+        
+  
+  return false;
     
 }
 
@@ -62,19 +64,16 @@ function displayResult() {
   var q9A = document.forms['quizForm']['q9'].value;
   var q10A = document.forms['quizForm']['q10'].value;
     
-  var resultOne = document.getElementById("resultOne");
-  var resultTwo = document.getElementById("resultTwo");
     
-    if (eScore > iScore) {
-        document.getElementById("quizForm").style.display="none";
-        resultOne.style.display="block";
-    } else if (eScore <= iScore) {
-        document.getElementById("quizForm").style.display="none";
-        resultTwo.style.display="block";
-    }
+ var resultsContainer = document.getElementById('quizResults');
+ resultsContainer.style.display = "block";
+    
+    
+    document.getElementById("quizForm").style.display="none";
 }
 
 function displayQuiz() {
+    
     document.getElementById("quizIntro").style.display = "none";
     document.getElementById("start").style.display = "none";
     document.getElementById("quizForm").style.display = "block";
